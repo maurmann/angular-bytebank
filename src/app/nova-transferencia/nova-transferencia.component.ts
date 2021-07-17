468d99ab14080ac404c28743b6532513ac1d9cb5
Adriano Maurmann <adriano.maurmann@ecs.com.br>
2021-07-16 22:47:24 -0300

    first commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+  valor:number;
+  destinatario:number;
+
+  transferir() {
+    console.log('transferir');
+    console.log(this.valor);
+    console.log(this.destinatario);
+  }
+
+}
+
